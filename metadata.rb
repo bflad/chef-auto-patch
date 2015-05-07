70300ec8c22b08bc334685651651ad3212589379
Brian Flad <bflad417@gmail.com>
2015-05-06 22:12:03 -0400

    Bump version to 0.1.10

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'auto-patch'
 maintainer 'Brian Flad'
-maintainer_email 'bflad@wharton.upenn.edu'
+maintainer_email 'bflad@417@gmail.com'
 license 'Apache 2.0'
 description 'Configures node for automatic patching.'
-version '0.1.9'
+version '0.1.10'
 recipe 'auto-patch', 'Configures node for automatic patching.'
 
 %w(cron).each do |d|
